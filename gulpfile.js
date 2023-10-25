179eb8b73ef62038e0aa3916c93645daf403e1bb
Max Glazunov <magus.mgl@gmail.com>
2023-10-25 22:29:45 +0500

    переименовал папку

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const {src, dest} = require('gulp')
 const concat = require('gulp-concat')
 
 const cssBundle = () =>
-    src('src/assets/style/**/*.css')
-.pipe(concat('style.css'))
+    src('src/assets/styles/**/*.css')
+.pipe(concat('styles.css'))
 .pipe(dest('dist/css'))
 
 exports.cssBundle = cssBundle
