84edb70a04026e7cdb54b933335ef3903d357da3
Max Glazunov <magus.mgl@gmail.com>
2023-10-27 14:04:44 +0500

    пофиксил открытие бургера и javascript

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import "./styles/main.scss"
 
-function openNav() {
-    const headerBurger = document.querySelector(".header__burger");
-    const headerNav = document.querySelector(".header__menu");
-    const body = document.querySelector("body");
+let headerBurger = document.querySelector(".header__burger");
+let headerNav = document.querySelector(".header__menu");
+let body = document.querySelector("body");
+
+headerBurger.addEventListener('click', () => {
     headerBurger.classList.toggle('change');
     headerNav.classList.toggle('change');
     body.classList.toggle('lock');
-}
+})
 
 document.addEventListener('click', (e) => {
     if (e.target.nodeName === 'A') {
-        openNav();
+        headerBurger.classList.toggle('change');
+        headerNav.classList.toggle('change');
+        body.classList.toggle('lock');
     }
 })
